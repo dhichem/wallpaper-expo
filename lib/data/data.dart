9c13e2ceaf0ab7b84224ace730b27c427415134f
dhichem <hichem.dimassi@gmail.com>
2021-09-29 23:10:30 +0200

    class categories added

---
+++ lib/data/data.dart
@@ -0,0 +1,8 @@
+import 'package:wallpaperapp/model/categories_model.dart';
+
+List<CatgeoriesModel> getCategories() {
+  List<CatgeoriesModel> categories =
+      List<CatgeoriesModel>.empty(growable: true);
+
+  CatgeoriesModel catgeoriesModel = new CatgeoriesModel();
+}
